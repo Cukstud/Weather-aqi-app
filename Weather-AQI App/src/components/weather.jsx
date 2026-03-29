ce4135405838949859d4ef1d0025682942dc68a1
N.V.S Likhit <95621128+Cukstud@users.noreply.github.com>
2026-03-29 15:15:39 +0530

    Add margin and padding to weather title

--- Weather-AQI App/src/components/weather.jsx
+++ Weather-AQI App/src/components/weather.jsx
@@ -2,7 +2,7 @@ function Weather({data}){
     if (!data) return null;
     return(
      <div>
-      <h1 className="text-5xl font-extrabold">{data.name}</h1>
+      <h1 className="text-5xl font-extrabold m-4 p-2">{data.name}</h1>
       <p className="text-1xl font-extrabold">Temperature: {data.main.temp} °C</p>
       <p className="text-1xl font-extrabold">Condition: {data.weather[0].description}</p>
     </div>
