4ef3a5ec23f66fa62d4a9f58f4e444a98dc1f5f2
N.V.S Likhit <95621128+Cukstud@users.noreply.github.com>
2026-03-29 09:35:41 +0530

    Implement backend
    Set up Express server with weather and AQI endpoints.

--- Weather-AQI App/Backend/server.js
+++ Weather-AQI App/Backend/server.js
@@ -1,1 +1,35 @@
+import express from 'express';
+import axios from 'axios'; 
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import { error } from 'console';
 
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.get('/weather',async(r,res)=>{
+    const {city} = r.query;
+    try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`);
+        res.json(response.data);
+    }
+    catch(error){
+        res.status(500).json({error:'Failed to fetch weather data'});
+    }
+});
+app.get('/aqi',async(r,res)=>{
+    const{lat,lon} = r.query;
+    try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+        res.json(response.data);
+    }
+    catch{
+        res.status(500).json({error:'Failed'});
+    }
+});
+app.listen(5000,()=>{
+    console.log('running')
+})
